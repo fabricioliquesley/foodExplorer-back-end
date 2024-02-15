d9df0f2d81bda420699e4d13296f5a28777b7c61
Fabricio <fabricioliquesley15@gmail.com>
2024-02-14 21:16:45 -0300

    feat: created the middleware to check if the user is authenticated

---
+++ src/middleware/ensureAuthenticated.js
@@ -0,0 +1,25 @@
+const { verify } = require("jsonwebtoken");
+const AppError = require("../utils/AppError");
+const authConfig = require("../config/auth");
+
+function ensureAuthenticated(request, response, next) {
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+        throw new AppError("JWT Token não informado", 401);
+    }
+
+    const [, token] = authHeader.split(" ");
+
+    try {
+        const {sub: user_id} = verify(token, authConfig.jwt.secret);
+
+        request.user = {
+            id: String(user_id)
+        }
+    } catch (error) {
+        throw new AppError("JWT Token inválido", 401);
+    }
+}
+
+module.exports = ensureAuthenticated;
