c250e7ba9625ebc5df51d01a013e910cad29bfa6
Fabricio <fabricioliquesley15@gmail.com>
2024-02-22 19:02:33 -0300

    feat: create columns in the orders table

--- src/database/knex/migrations/20240214001849_createOrdersTable.js
+++ src/database/knex/migrations/20240214001849_createOrdersTable.js
@@ -1,9 +1,12 @@
 exports.up = knex => knex.schema.createTable("orders", table => {
 	table.text("id").primary();
-    table.text("number").notNullable();
-    table.integer("total").notNullable();
+    table.text("status").notNullable();
+    table.integer("codigo").notNullable();
+    table.text("details").notNullable();
     table.integer("meal_id").references("id").inTable("meals").onDelete("CASCADE");
     table.integer("user_id").references("id").inTable("users").onDelete("CASCADE");
+    
+    table.timestamp("created_at").default(knex.fn.now());
 });
 
 exports.down = knex => knex.schema.dropTable("orders");
