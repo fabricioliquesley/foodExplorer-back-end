32deb9a34b7cdfb768216f1a404f4b46a31e1a0c
Fabricio <fabricioliquesley15@gmail.com>
2024-02-22 21:55:57 -0300

    feat: rename the codigo column to code

--- src/database/knex/migrations/20240214001849_createOrdersTable.js
+++ src/database/knex/migrations/20240214001849_createOrdersTable.js
@@ -1,9 +1,8 @@
 exports.up = knex => knex.schema.createTable("orders", table => {
 	table.text("id").primary();
     table.text("status").notNullable();
-    table.integer("codigo").notNullable();
+    table.integer("code").notNullable();
     table.text("details").notNullable();
-    table.integer("meal_id").references("id").inTable("meals").onDelete("CASCADE");
     table.integer("user_id").references("id").inTable("users").onDelete("CASCADE");
     
     table.timestamp("created_at").default(knex.fn.now());
