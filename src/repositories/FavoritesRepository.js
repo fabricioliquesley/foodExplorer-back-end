c313df6b298a2ede7fcead51b406173a0d7efed1
Fabricio <fabricioliquesley15@gmail.com>
2024-03-09 12:07:40 -0300

    feat: create a method to delete a favorite of a user in database

--- src/repositories/FavoritesRepository.js
+++ src/repositories/FavoritesRepository.js
@@ -5,8 +5,16 @@ class FavoritesRepository {
         return await knex("favorites").insert(data);
     }
 
-    async getFavorites(user_id) {
+    async getFavorites(user_id, favorites_id) {
+        if (favorites_id) {
+            return await knex("favorites").where("id", favorites_id).first();
+        }
+
         return await knex("favorites").where("user_id", user_id);
+    }
+
+    async delete(favorites_id) {
+        await knex("favorites").delete().where("id", favorites_id);
     }
 }
 
