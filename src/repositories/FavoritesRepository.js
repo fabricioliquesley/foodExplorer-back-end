761ebf0a3fb3a00af743bd25d18c981773651ea1
Fabricio <fabricioliquesley15@gmail.com>
2024-03-09 11:05:45 -0300

    feat: create method to fetch for a user favorites

--- src/repositories/FavoritesRepository.js
+++ src/repositories/FavoritesRepository.js
@@ -4,6 +4,10 @@ class FavoritesRepository {
     async create(data) {
         return await knex("favorites").insert(data);
     }
+
+    async getFavorites(user_id) {
+        return await knex("favorites").where("user_id", user_id);
+    }
 }
 
 module.exports = FavoritesRepository;
