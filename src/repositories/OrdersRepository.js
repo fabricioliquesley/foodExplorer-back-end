6249538b2d9420ed604b2c9c3b459c1342c6891b
Fabricio <fabricioliquesley15@gmail.com>
2024-03-09 13:16:43 -0300

    feat: create method to edit the status of an order in the database

--- src/repositories/OrdersRepository.js
+++ src/repositories/OrdersRepository.js
@@ -8,6 +8,10 @@ class OrdersRepository {
     async fetchOrders(user_id){
         return knex("orders").where("user_id", user_id).orderBy("created_at");
     }
+
+    async editOrder({ status, orderId }) {
+        return knex("orders").update({status}).where("id", orderId);
+    }
 }
 
 module.exports = OrdersRepository;
