18fbd01700e91816875dba887b23a21938f9e492
Fabricio <fabricioliquesley15@gmail.com>
2024-03-13 16:06:03 -0300

    feat: create route to view an image saved in the database

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ require("express-async-errors");
 
 const AppError = require("./utils/AppError");
 const express = require("express");
-
 const app = express();
-const routes = require("./routes/index");
+
+const routes = require("./routes");
+const uploadConfig = require("./config/upload");
 
 app.use(express.json());
 
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 app.use(routes);
 
 app.use((error, request, response, next) => {
