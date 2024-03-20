eaf4c45e524b1c67192b8677d037d175fdf01ffe
Fabricio <fabricioliquesley15@gmail.com>
2024-03-19 21:31:00 -0300

    feat: add logic to fetch all orders when the user is an administrator

--- src/repositories/OrdersRepository.js
+++ src/repositories/OrdersRepository.js
@@ -1,16 +1,28 @@
 const knex = require("../database/knex");
 
 class OrdersRepository {
-    async create(order){
+    async create(order) {
         await knex("orders").insert(order);
     }
 
-    async fetchOrders(user_id){
-        return knex("orders").where("user_id", user_id).orderBy("created_at");
+    async fetchUser(user_id) {
+        return await knex("users").where("id", user_id).first();
+    }
+
+    async fetchOrders(user_id, isUserAdmin) {
+        let orders;
+
+        if (isUserAdmin) {
+            orders = knex("orders").orderBy("created_at");
+        } else {
+            orders = knex("orders").where("user_id", user_id).orderBy("created_at");
+        }
+
+        return orders
     }
 
     async editOrder({ status, orderId }) {
-        return knex("orders").update({status}).where("id", orderId);
+        return knex("orders").update({ status }).where("id", orderId);
     }
 }
 
